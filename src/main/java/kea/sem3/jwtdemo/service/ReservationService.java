4347bf7e25252a2d39c7d2a054f46937ec8ebeac
andreajiao <andreajiao@gmail.com>
2022-05-04 11:16:01 +0200

    resevationService

---
+++ src/main/java/kea/sem3/jwtdemo/service/ReservationService.java
@@ -0,0 +1,11 @@
+package kea.sem3.jwtdemo.service;
+
+import kea.sem3.jwtdemo.repositories.ReservationRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReservationService {
+    ReservationRepository reservationRepository;
+
+
+}
