135b2d993f97269d383192f33a67e07cc474dc3f
Casper <casper.aakviststeengaard@gmail.com>
2022-05-06 11:03:18 +0200

    Fixing database setup and Movies CRUD funktion

--- src/main/java/kea/sem3/jwtdemo/configuration/MakeTestData.java
+++ src/main/java/kea/sem3/jwtdemo/configuration/MakeTestData.java
@@ -2,6 +2,8 @@ package kea.sem3.jwtdemo.configuration;
 
 import kea.sem3.jwtdemo.entity.*;
 import kea.sem3.jwtdemo.repositories.MovieRepository;
+import kea.sem3.jwtdemo.repositories.ReservationRepository;
+import kea.sem3.jwtdemo.security.UserRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Profile;
@@ -17,10 +19,13 @@ public class MakeTestData implements ApplicationRunner {
 
 
     MovieRepository movieRepository;
-
+    //UserRepository userRepository;
     //public MakeTestData(UserRepository userRepository) {
     // this.userRepository = userRepository;
     //}
+    public MakeTestData(MovieRepository movieRepository) {
+       this.movieRepository = movieRepository;
+    }
 
     public  void makePlainTestData(){
         /*
@@ -32,14 +37,15 @@ public class MakeTestData implements ApplicationRunner {
         userRepository.save(admin);
         userRepository.save(both);
 
+    public Movie(String title, String genre, int price, String description, String rating, int ageLimit) {
 
          */
-        Movie movie1 = movieRepository.save(new Movie("The Green Man", "Horror"));
-        Movie movie2 = movieRepository.save(new Movie("Some Women Dancing", "Comedy"));
-        Movie movie3 = movieRepository.save(new Movie("In Captivity", "Thriller"));
-        Movie movie4 = movieRepository.save(new Movie("The Love Birds", "Romance"));
-        Movie movie5 = movieRepository.save(new Movie("Angry People Watching", "Action"));
-        Movie movie6 = movieRepository.save(new Movie("Space Walking", "Science Fiction"));
+        movieRepository.save(new Movie("The Green Man", "Horror", 80, "This is a description", "PG-13", 13));
+        movieRepository.save(new Movie("Some Women Dancing", "Comedy", 80, "This is a description", "PG", 7));
+        movieRepository.save(new Movie("In Captivity", "Thriller", 80, "This is a description", "R", 18));
+        movieRepository.save(new Movie("The Love Birds", "Romance", 80, "This is a description", "PG-13", 13));
+        movieRepository.save(new Movie("Angry People Watching", "Action", 80, "This is a description", "PG-13", 13));
+        movieRepository.save(new Movie("Space Walking", "Science Fiction", 80, "This is a description", "NC-17", 21));
 
 
         System.out.println("########################################################################################");
--- src/main/java/kea/sem3/jwtdemo/entity/Movie.java
+++ src/main/java/kea/sem3/jwtdemo/entity/Movie.java
@@ -12,7 +12,9 @@ import org.springframework.boot.convert.DurationFormat;
 import javax.persistence.*;
 import java.time.Duration;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -34,14 +36,22 @@ public class Movie{
     public Movie() {}
 
 
-    public Movie(String title, String category) {
+    public Movie(String title, String genre, int price, String description, String rating, int ageLimit) {
         this.title = title;
-        this.genre = category;
+        this.genre = genre;
+        this.price = price;
+        this.description = description;
+        this.rating = rating;
+        this.ageLimit = ageLimit;
     }
 
     public Movie(MovieRequest body){
         this.title = body.getTitle();
         this.genre = body.getGenre();
+        this.price = body.getPrice();
+        this.description = body.getDescription();
+        this.rating = body.getRating();
+        this.ageLimit = body.getAgeLimit();
     }
     @Column(length = 60)
     String title;
@@ -49,11 +59,23 @@ public class Movie{
     @Column(length = 60)
     String genre;
 
+    int price;
+
+    int ageLimit;
+
+    @Column(length = 10)
+    String rating;
+
+    @Column(length = 500)
+    String description;
+
+
     @CreationTimestamp
     LocalDateTime created;
 
     @UpdateTimestamp
     LocalDateTime edited;
+
 
 
     /*
